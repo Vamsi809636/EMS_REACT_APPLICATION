90784804fec2bc6aee9464632bebb4c0b74527c3
Vamsi809636 <vamsikrishnavalluri49@gmail.com>
2026-05-15 03:13:06 +0530

    .

--- src/types/role.types.ts
+++ src/types/role.types.ts
@@ -2,11 +2,11 @@ export interface Role {
   id: string;
   name: string;
   description?: string;
-  level: number;
+ 
 }
 
 export interface RoleRequest {
   name: string;
   description?: string;
-  level: number;
+  
 }
